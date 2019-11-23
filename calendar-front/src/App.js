9f8a05845023d40bb66192b89b614bc0f0954a7d
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-23 17:02:43 +0200

    added login functionality to frontend

--- calendar-front/src/App.js
+++ calendar-front/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import loginService from './services/login'
+import LoginForm from './components/LoginForm'
+import { useField } from './hooks'
 
-function App() {
+const App = (props) => {
+  const username = useField('username')
+  const password = useField('password')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    const credentials = {
+      username: username.value,
+      password: password.value
+    }
+
+    try {
+      // console.log('credentials', credentials)
+      const user = await loginService.login(
+        credentials
+      )
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+
+      setUser(user)
+      username.reset('')
+      password.reset('')
+    } catch (exception) {
+      console.log('käyttäjätunnus tai salasana virheellinen')
+    }
+  }
+
+  const omitReset = (hook) => {
+    let { reset, ...hookWithoutReset } = hook
+    // console.log('hookWitoutReset', JSON.stringify(hookWithoutReset))
+    return hookWithoutReset
+  }
+
+  const handleLogout = async (event) => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    // notify(`${user.username} logged out`, false)
+    setUser(null)
+  }
+
+  if (user) {
+    return (
+      <div>
+        <h2>Tanssipaikat</h2>
+
+        <p>{`Logged in as ${user.name}`}</p>
+        <button onClick={() => handleLogout()}>logout</button>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <LoginForm className='loginform'
+        username={omitReset(username)}
+        password={omitReset(password)}
+
+        handleSubmit={handleLogin}
+      />
     </div>
-  );
+
+  )
 }
 
 export default App;
