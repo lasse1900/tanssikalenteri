53d44685c93f7d74c4e39c3bd4f35667a3a4916a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-11-24 18:11:31 +0200

    frontend basic page layout

--- calendar-front/src/App.js
+++ calendar-front/src/App.js
@@ -1,7 +1,52 @@
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'  // Redirect
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
 import { useField } from './hooks'
+import './App.css'
+
+
+const Tanssikalenteri = () => {
+  return (
+    <div>
+      <h2>Tanssikalenteri</h2>
+    </div>
+  );
+}
+
+const Tanssipaikat = () => {
+  return (
+    <div>
+      <h2>Tanssipaikat</h2>
+    </div>
+  );
+}
+
+const Tanssikoulut = () => {
+  return (
+    <div>
+      <h2>Tanssikoulut</h2>
+    </div>
+  );
+}
+
+const Kalenteri = () => {
+  return (
+    <div>
+      <h2>Kalenteri</h2>
+    </div>
+  );
+}
+
+const Videolinkit = ({ match }) => {
+  return (
+    <div>
+      <h2>Tanssivideot</h2>
+      <a target='_blank' rel="noopener noreferrer" href= "https://www.youtube.com/watch?v=2iR_XlfBPpI">"arg. tango"</a>
+    </div>
+  )
+}
+
 
 const App = (props) => {
   const username = useField('username')
@@ -52,28 +97,41 @@ const App = (props) => {
     setUser(null)
   }
 
-  if (user) {
+  if (user === null) {
     return (
       <div>
-        <h2>Tanssipaikat</h2>
-
-        <p>{`Logged in as ${user.name}`}</p>
-        <button onClick={() => handleLogout()}>logout</button>
+        <LoginForm className='loginform'
+          username={omitReset(username)}
+          password={omitReset(password)}
+          handleSubmit={handleLogin}
+        />
       </div>
     )
   }
 
   return (
     <div>
+      <Router>
+        <div>
+          <ul>
+            <Link className="link" to="/">Tanssikalenteri</Link>
+            <Link className="link" to="/tanssipaikat">Tanssipaikat</Link>
+            <Link className="link" to="/tanssikoulut">Tanssikoulut</Link>
+            <Link className="link" to="/kalenteri">Kalenteri</Link>
+            <Link className="link" to="/videolinkit">Videolinkit</Link>
+          </ul>
+          <hr />
 
-      <LoginForm className='loginform'
-        username={omitReset(username)}
-        password={omitReset(password)}
-
-        handleSubmit={handleLogin}
-      />
+          <Route exact path="/" component={Tanssikalenteri} />
+          <Route exact path="/tanssipaikat" component={Tanssipaikat} />
+          <Route exact path="/tanssikoulut" component={Tanssikoulut} />
+          <Route exact path="/kalenteri" component={Kalenteri} />
+          <Route path="/videolinkit" component={Videolinkit} />
+        </div>
+      </Router>
+      <p>{`Logged in as ${user.name}`}</p>
+      <button onClick={() => handleLogout()}>logout</button>
     </div>
-
   )
 }
 
