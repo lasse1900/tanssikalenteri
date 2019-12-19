cc78d1ce3cc9b8def09412803f1e4d51f691c4a2
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-12-19 15:05:58 +0200

    corrected bug with Redux usage in LoginForm

--- calendar-front/src/components/LoginForm.js
+++ calendar-front/src/components/LoginForm.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { useField } from '../hooks'
 // import PropTypes from 'prop-types'
 import loginService from '../services/login'
-import { connect } from 'react-redux'
 import { loginUser, setUser } from '../reducers/userReducer'
 import { setMessage } from '../reducers/notificationReducer'
 import '../app.css'
@@ -17,7 +17,7 @@ const LoginForm = (props) => {
     const loggedUserJSON = window.localStorage.getItem('loggedBallroomappUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
-      setUser(user)
+      props.setUser(user)
     }
   }, [])
 
@@ -37,15 +37,15 @@ const LoginForm = (props) => {
         credentials
       )
       window.localStorage.setItem('loggedBallroomappUser', JSON.stringify(user))
-
-      setUser(user)
+      props.setUser(user)
+      props.notify('login succeeded', false)
       console.log('asetettu käyttäjä', user)
       username.reset('')
       password.reset('')
-      props.notify('login succeeded', false)
       props.history.push('/')
     } catch (exception) {
       setShowInfo(' username or password incorrect!')
+      // console.log('virhe', exception)
       props.notify('login not succeeded', false)
     }
   }
@@ -81,4 +81,4 @@ LoginForm.propTypes = {
   // password: PropTypes.object.isRequired
 }
 
-export default connect(null, { loginUser, setMessage })(withRouter(LoginForm))
+export default connect(null, { loginUser, setUser, setMessage })(withRouter(LoginForm))
