55a7cbfdee8e164fc4e42537ea240f53ca2ca3f5
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-01-12 16:26:18 +0200

    small chnge to services/users.js concerning post & headers

--- calendar-front/src/services/users.js
+++ calendar-front/src/services/users.js
@@ -3,11 +3,6 @@ const baseUrl = '/api/users'
 
 let token = null
 
-// const setToken = (newToken) => {
-//   token = `Bearer ${newToken}`
-//   console.log('users.js - token', token)
-// }
-
 const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
@@ -23,19 +18,10 @@ const create = async (newUser) => {
   return response.data
 }
 
-// const post = async (uri, data, headers) => {
-//   const config = { headers: { ...headers, Authorization: token } }
-//   console.log('post /services/users.js:', uri, data, headers)
-//   const result = await axios.post(uri, data, config)
-//   return result.data
-// }
-
 const post = async (uri, data) => {
   console.log('post /services/users.js:', uri, data)
   const result = await axios.post(uri, data)
   return result.data
 }
 
-
-// export default { getAll, setToken, create, post }
 export default { getAll, create, post }
