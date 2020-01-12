89865be3ad85f3bec4a9ef63fa4ec6940c1dcef0
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-01-12 22:00:55 +0200

    starting Backend with Danceschool controller step1c

---
+++ calendar-front/src/components/DanceSchools.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DanseSchools = () => {
+  return (
+    <div>
+      <h3>Danse Schools</h3>
+    </div>
+  );
+}
+
+export default DanseSchools;
--- calendarBackend/models/ballroom2.js
+++ calendarBackend/models/ballroom2.js
@@ -5,6 +5,7 @@ const ballroomSchema2= mongoose.Schema({
   title: String,
   author: String,
   url: String,
+  likes: Number,
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
