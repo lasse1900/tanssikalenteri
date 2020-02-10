75fd8e4e06986c83279b683d3fbd1eed79496e19
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-10 19:21:48 +0200

    put extra route back and build exclude in .gitignore

---
+++ calendarBackend/build/precache-manifest.397506e10a4d1f817dc5a68b23d82724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2606e591e93b5c4db8af",
+    "url": "/static/css/main.f054ed84.chunk.css"
+  },
+  {
+    "revision": "2606e591e93b5c4db8af",
+    "url": "/static/js/main.2606e591.chunk.js"
+  },
+  {
+    "revision": "6fff1a4a4d4766e10206",
+    "url": "/static/css/1.66ef7ff2.chunk.css"
+  },
+  {
+    "revision": "6fff1a4a4d4766e10206",
+    "url": "/static/js/1.6fff1a4a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70ea742f219141ea8807bb8d068842cf",
+    "url": "/static/media/argentine-tango_primary.70ea742f.jpg"
+  },
+  {
+    "revision": "2665569c576f84baa80b460832bb6dee",
+    "url": "/index.html"
+  }
+];
